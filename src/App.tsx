f8cce76a29a7b2d5bbdc216d8e0167d6d7de81af
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-10-31 22:30:30 +0100

    added basic visualization example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ForceGraph2D from 'react-force-graph-2d';
+import ForceGraph3D from 'react-force-graph-3d';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [data, setData] = useState(undefined)
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      "logicalCoupling": false,
+      "semanticCoupling": true,
+      "contributorCoupling": false,
+      "numServices": 2,
+      "intervalSeconds": 3600,
+      "sizeThreshold": 15
+    })
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/decompositions/decompose/1689/visualization", requestOptions)
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      {
+        data == undefined ? <h1>data loading...</h1> :
+          <ForceGraph3D
+            backgroundColor="black"
+            linkColor={_ => "white"}
+            linkWidth={3}
+            nodeColor={_ => "lightgreen"}
+            nodeRelSize={8}
+            nodeLabel="group"
+            graphData={data} />
+      }
     </div>
   );
 }
