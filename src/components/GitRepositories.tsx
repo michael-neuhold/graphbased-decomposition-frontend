d71808b417768cda44d009874c201c075d473ae7
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-11-05 10:54:23 +0100

    implemented add repository in frontend

--- src/components/GitRepositories.tsx
+++ src/components/GitRepositories.tsx
@@ -1,5 +1,5 @@
 import { Button, Heading, Pane, Table, TextInput } from "evergreen-ui"
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { Configuration, GitRepository, RepositoryControllerImplApi, RepositoryControllerImplApiFactory } from "../api"
 
 
@@ -8,15 +8,28 @@ const config = new Configuration();
 export const GitRepositories = () => {
 
   const [repositories, setRepositories] = useState<GitRepository[]>([])
-  
+  const [newRepository, setNewRepository] = useState("")
+  const [addedRepository, setAddedRepository] = useState(0)
+
+
+  const addNewRepository = () => {
+    const addRepository = async () => {
+      const api = new RepositoryControllerImplApi(config, "http://localhost:8080");
+      const response = api.addRepositoryUsingPOST({uri: newRepository})
+    }
+    addRepository().catch(console.error);
+    setAddedRepository(addedRepository + 1);
+    setNewRepository("");
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const api = new RepositoryControllerImplApi(config, "http://localhost:8080");
       const repos = await api.getAllRepositoriesUsingGET();
       setRepositories(repos.data);
     }
-   fetchData().catch(console.error);
-  }, [])
+    fetchData().catch(console.error);
+  }, [addedRepository])
 
   return (
     <Pane>
@@ -25,12 +38,12 @@ export const GitRepositories = () => {
       </Heading>
 
       <Pane paddingBottom={10}>
-        <TextInput name="repository-url-input" placeholder="https://github.com/denver-code/nftgenerator" />
-        <Button marginLeft={16} appearance="primary">
+        <TextInput name="repository-url-input" placeholder="https://github.com/denver-code/nftgenerator" value={newRepository} onChange={(e: any) => setNewRepository(e.target.value)} />
+        <Button marginLeft={16} appearance="primary" onClick={addNewRepository}>
           Add
         </Button>
       </Pane>
-      
+
       <Table>
         <Table.Head>
           <Table.TextHeaderCell>Id</Table.TextHeaderCell>
