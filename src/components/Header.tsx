f201491937575aa2e276007aa8bfcc01d7c8fce4
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-11-09 22:54:03 +0100

    style fixes and linked (local) swagger documentation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-import { Heading, Pane } from "evergreen-ui"
+import { Heading, Link, Pane } from "evergreen-ui"
 
 export const Header = () => {
   return (
-    <Pane padding={16}>
-      <Heading size={500}>
+    <Pane padding={24}>
+      <Heading size={600}>
         Graphbased decomposition
       </Heading>
+      <Link href="http://localhost:8080/swagger-ui.html" target="blank">Swagger UI</Link>
     </Pane>
   )
 }
